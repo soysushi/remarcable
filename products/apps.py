29b89f29bd20dbb950baee19c8124a416ae89c28
Oscar Chi <soysushi@gmail.com>
2026-02-20 17:30:17 -0800

    signals for slug- decoupling

--- products/apps.py
+++ products/apps.py
@@ -1,6 +1,12 @@
+"""Products app configuration."""
 from django.apps import AppConfig
 
 
 class ProductsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'products'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.products"
+    verbose_name = "Products"
+
+    def ready(self):
+        """Import signal handlers when app is ready."""
+        from . import signals 
